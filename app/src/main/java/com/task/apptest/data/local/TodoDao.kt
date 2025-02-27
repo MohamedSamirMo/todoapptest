0b8b065404e25d7e7a00ff9e7eab33fc67a16a4f
mohamed samir <75226676+MohamedSamirMo@users.noreply.github.com>
2025-02-27 11:36:08 +0200

    fixed all mistakes

--- app/src/main/java/com/task/apptest/data/local/TodoDao.kt
+++ app/src/main/java/com/task/apptest/data/local/TodoDao.kt
@@ -4,7 +4,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import kotlinx.coroutines.flow.Flow
+
 @Dao
 interface TodoDao {
     @Query("SELECT * FROM todos")
@@ -13,15 +13,10 @@ interface TodoDao {
     @Query("SELECT * FROM todos WHERE id = :id")
     fun getTodoById(id: Int): TodoEntity?
 
-    @Query("SELECT * FROM todos LIMIT :limit")
-    fun getLimitedTodos(limit: Int): List<TodoEntity>
-
     @Query("SELECT * FROM todos WHERE title LIKE '%' || :query || '%'")
     fun searchTodos(query: String): List<TodoEntity>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
      fun insertTodos(todos: List<TodoEntity>)
 
-    @Query("DELETE FROM todos")
-     fun clearTodos()
 }
